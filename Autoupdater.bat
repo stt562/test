c8b852fa713ecf028b957cad1d5cd22f3ee602fb
stt562 <stt3543@gmail.com>
2024-12-16 16:48:18 +0100

    Update Autoupdater.bat

--- Autoupdater.bat
+++ Autoupdater.bat
@@ -7,4 +7,9 @@ cls
 echo Updating...
 timeout 10 >nul /nobreak
 
-curl 
+
+echo timeout 10>>tempfile.cmd
+echo curl https://raw.githubusercontent.com/stt562/test/refs/heads/main/Autoupdater.bat>>Autoupdater.bat>>tempfile.cmd
+echo del tempfile.cmd>>tempfile.cmd
+
+start tempfile.cmd
