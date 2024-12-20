ffed60ae75909bb43f306c23766b7e9737f6cbd2
stt562 <stt3543@gmail.com>
2024-12-20 15:39:45 +0100

    Update Autoupdater.bat

--- Autoupdater.bat
+++ Autoupdater.bat
@@ -4,7 +4,7 @@
 cls
 timeout 10
 cls
-echo Updating... & curl https://raw.githubusercontent.com/stt562/test/refs/heads/main/Autoupdater.bat>Autoupdater.bat
+echo Updating... & echo: & curl https://raw.githubusercontent.com/stt562/test/refs/heads/main/Autoupdater.bat>Autoupdater.bat
 
 cls
 echo Updated!
