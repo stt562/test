6afa076b992a040607252800d740ed4e8a6920c0
stt562 <stt3543@gmail.com>
2024-12-20 15:55:35 +0100

    Update Autoupdater.bat

--- Autoupdater.bat
+++ Autoupdater.bat
@@ -2,13 +2,13 @@
 @echo off
 @color 9f
 cls
-set /a version=Alpha
+set /a ver=Alpha
 timeout 10
 cls
 echo Updating... & echo: & curl https://raw.githubusercontent.com/stt562/test/refs/heads/main/Autoupdater.bat>Autoupdater.bat
-time /T>>time.txt
-set /a t=<time.txt
-del time.txt
+time /T>t.txt
+set /a t=<t.txt
+del t.txt
 cls
 echo Updated!
 echo:
@@ -16,7 +16,7 @@ echo Infos to the Update
 echo:
 echo -------------------
 echo:
-echo Version: %version%
+echo Version: %ver%
 echo Updated at %t%
 timeout 10
 cls
