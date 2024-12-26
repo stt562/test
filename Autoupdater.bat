0b4cd61d2459beab9ae41c368e256d7a31215d6a
stt562 <stt3543@gmail.com>
2024-12-26 12:29:04 +0100

    Alpha

--- Autoupdater.bat
+++ Autoupdater.bat
@@ -2,10 +2,10 @@
 @echo off
 @color 9f
 cls
-set "ver=Alpha"
 timeout 10
 cls
 echo Updating... & echo: & curl https://raw.githubusercontent.com/stt562/test/refs/heads/main/Autoupdater.bat>Autoupdater.bat
+set "ver=Alpha"
 cls
 echo Updated!
 echo:
